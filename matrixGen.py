e73c831765bdd37021a71c8d511f398368bbd545
11ray <raruidol@dsic.upv.es>
2019-03-21 11:59:59 +0100

    diccionario reestructurado para la matriz de J x Steps

--- matrixGen.py
+++ matrixGen.py
@@ -3,32 +3,37 @@ import numpy as np
 
 dict={}
 dict['target'] = {}
-dict['talk'] = {}
-dict['vote'] = {}
 
+day = 0
 with open('001/000.log', 'r') as f:
     while True:
         line = f.readline().split(',')
         if line == ['']:
             break
 
+        # Almaceno los roles de cada agente (id)
         if line[1] == 'status':
             dict['target'][line[2]] = line[3]
 
-        elif line[1] == 'talk':
-            if int(line[4])-1 in dict['talk']:
-                dict['talk'][int(line[4]) - 1].append(line[5])
-            else:
-                dict['talk'][int(line[4]) - 1] = []
-                dict['talk'][int(line[4])-1].append(line[5])
+        # Genero nuevas claves para un nuevo dia de la partida
+        if day != int(line[0]):
+            day = int(line[0])
+            dict[day] = {}
+            dict[day]['talk'] = {}
+            dict [day]['vote'] = {}
 
-        elif line[1] == 'vote':
-
-            if int(line[2])-1 in dict['vote']:
-                dict['vote'][int(line[2]) - 1][int(line[3]) - 1] += 1
+        # Almaceno la sucesión de mensajes de cada agente para cada dia
+        if line[1] == 'talk' and day!=0:
+            if int(line[4]) in dict[day]['talk']:
+                dict[day]['talk'][int(line[4])].append(line[5])
             else:
-                dict['vote'][int(line[2]) - 1] = [0, 0, 0, 0, 0]
-                dict['vote'][int(line[2])-1][int(line[3]) - 1] += 1
+                dict[day]['talk'][int(line[4])] = []
+                dict[day]['talk'][int(line[4])].append(line[5])
+
+        # Almaceno la votación realizada al finalizar cada dia
+        elif line[1] == 'vote' and day != 0:
+            dict[day]['vote'][int(line[2])] = int(line[3])
+
 
 
 
