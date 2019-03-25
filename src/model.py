58d1ac414a4496ce7309a9882aa46a81ec8996de
jairsan <jairsan@inf.upv.es>
2019-03-25 08:35:58 +0100

    ...

--- src/model.py
+++ src/model.py
@@ -7,6 +7,8 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.rnn = nn.GRU(batch_first=True,input_size=net_params["n_features"],hidden_size=net_params["hidden_size"])
         self.att_softmax = nn.Softmax(dim=1)
+        self.linear1 = nn.Linear(net_params["hidden_size"], net_params["linear_hidden_size"])
+        self.linear2 = nn.Linear(net_params["linear_hidden_size"],net_params["n_roles"])
 
     def forward(self, x):
         #Receives as input a  Tensor of shape (J,Steps,N_features)
@@ -22,12 +24,19 @@ class Net(nn.Module):
         V = i.permute(0,2,1)
         K = i
 
+        #Por aquí falta un /sqrt(dim)
         attention_intermediate = torch.matmul(K,Q)
         attention_weights = self.att_softmax(attention_intermediate)
-
         attention_out = torch.matmul(V,attention_weights)
 
-        print(attention_out.shape)
+        i = attention_out.permute(0,2,1)
+
+
+        i = self.linear1(i)
+        #Meter activation
+        out = self.linear2(i)
+
+        print(out.shape)
 
         #Need to put back to correct shape
 
--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import torch
 from src import model
 
 
-net = model.Net({"n_features":512,"hidden_size":128})
+net = model.Net({"n_features":512,"hidden_size":128,"linear_hidden_size":32,"n_roles":7})
 
 
 x = torch.randn((1,15,20,512))
