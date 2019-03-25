24591d22fa1330442980063d3e972bac265fd617
jairsan <jairsan@inf.upv.es>
2019-03-25 21:51:04 +0100

    First working model version

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class Net(nn.Module):
 
@@ -7,48 +8,51 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.rnn = nn.GRU(batch_first=True,input_size=net_params["n_features"],hidden_size=net_params["hidden_size"])
         self.att_softmax = nn.Softmax(dim=1)
+        self.att_denom = torch.sqrt(torch.tensor(net_params["hidden_size"], dtype=torch.float32))
         self.linear1 = nn.Linear(net_params["hidden_size"], net_params["linear_hidden_size"])
         self.linear2 = nn.Linear(net_params["linear_hidden_size"],net_params["n_roles"])
 
     def forward(self, x):
-        #Receives as input a  Tensor of shape (J,Steps,N_features)
+        # Receives as input a  Tensor of shape (J,Steps,N_features)
+        # If it instead was (1,...), you can do x.view(x.size()[1],x.size()[2],x.size()[3]) to remov the 1 direction
+
+        # Since we use Batch first, J acts in place of the batch
         i, _ = self.rnn(x)
 
-
+        # Now we exchange J and steps, resulting in so (Steps,J,N_features)
+        # At the output of each step, we wish to apply self attention
+        i = i.permute(1,0,2)
 
         # For k,q,v in columnvector, ATT = V softmax(K^T Q)
-        i = i.permute(1,0,2)
         #We transpose Q to be of shape (Steps,N_features,J)
         Q = i.permute(0,2,1)
         #Same with V
         V = i.permute(0,2,1)
+        #K is already in the right shape
         K = i
 
-        #Por aquí falta un /sqrt(dim)
-        attention_intermediate = torch.matmul(K,Q)
+        # Self attention
+        attention_intermediate = torch.div( torch.matmul(K,Q), self.att_denom)
         attention_weights = self.att_softmax(attention_intermediate)
         attention_out = torch.matmul(V,attention_weights)
 
+
+        #We now move to (Steps,J,N_features) in order to apply the linear layers
         i = attention_out.permute(0,2,1)
 
-
+        # First linear
         i = self.linear1(i)
-        #Meter activation
-        out = self.linear2(i)
+        i = F.relu(i)
 
-        print(out.shape)
-
-        #Need to put back to correct shape
+        i = self.linear2(i)
 
 
+        #Back to (J,Steps,N_features)
+        out = i.permute(1,0,2)
+
+        return out
 
 
-
-
-
-
-
-        #K = i
 
 
 
--- train.py
+++ train.py
@@ -1,10 +1,76 @@
 import torch
+import torch.utils.data as data
+import torch.nn.functional as F
+import torch.optim as optim
+
 from src import model
 
 
-net = model.Net({"n_features":512,"hidden_size":128,"linear_hidden_size":32,"n_roles":7})
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
 
 
-x = torch.randn((1,15,20,512))
+net = model.Net({"n_features":512,"hidden_size":128,"linear_hidden_size":32,"n_roles":7}).to(device)
 
-net.forward(x.view(x.size()[1],x.size()[2],x.size()[3]))
+
+exp_steps = 20
+exp_players = 15
+
+X = torch.randn((1,exp_players,exp_steps,512))
+
+Y = torch.randint(1,7,(1,exp_players))
+
+VALID_LOSS = torch.round(torch.rand((1,exp_steps)))
+
+
+
+train_dataset = data.TensorDataset(X,Y,VALID_LOSS)
+train_dataloader = data.DataLoader(train_dataset)
+
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(100):
+    print('Epoch {}/{}'.format(epoch, 100 - 1))
+    print('-' * 10)
+    # Training
+
+
+    running_loss = 0.0
+    running_corrects = 0
+
+
+    for x, y, valid in train_dataloader:
+
+        x = x.view(x.size()[1],x.size()[2],x.size()[3])
+        y = y.view(y.size()[1])
+        valid = valid.view(valid.size()[1],-1)
+
+
+        #Valid is expanded, in blocks of J that hold the same value
+        valid = valid.expand(-1,exp_players).contiguous().view(-1)
+
+        #Repeat targets across timesteps
+        y = y.repeat(exp_steps)
+
+        x, y, valid = x.to(device), y.to(device), valid.to(device)
+
+        #Fix valid somewhere along here
+
+        optimizer.zero_grad()
+        net_output = net.forward(x)
+
+
+        net_output = net_output.permute(1,0,2).contiguous().view(-1,7)
+
+        loss = F.cross_entropy(net_output, y,reduction='none')
+
+        cost = loss.sum()
+
+        print(cost.double())
+
+        cost.backward()
+        optimizer.step()
+
+
+
+
