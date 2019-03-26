3ec2acf618c0daec3da60e99cc216aa04ffc0a65
Javier Iranzo <jairsan@upv.es>
2019-03-26 18:12:32 +0100

    Add dataset stuff

---
+++ data/synthetic/synthetic_train/generate_set.sh
@@ -0,0 +1,3 @@
+for i in {0..999}; do
+echo $PWD/$i >> set.csv
+done
---
+++ src/dataset.py
@@ -0,0 +1,25 @@
+import torch
+import torch.utils.data as data
+import pandas as pd
+import numpy as np
+
+class WerewolfDataset(data.Dataset):
+  """Werewolf dataset."""
+
+  def __init__(self, name_filelist ):
+    self.name_dataframe = pd.read_csv(name_filelist)
+
+
+  def __len__(self):
+    return len(self.name_dataframe)
+
+  def __getitem__(self, idx):
+    basename = self.name_dataframe.iloc[idx][0]
+
+
+    x = torch.from_numpy(np.load(basename+".x.npy"))
+    y = torch.from_numpy(np.load(basename + ".y.npy"))
+    valid = torch.from_numpy(np.load(basename + ".valid.npy"))
+
+
+    return (x,y,valid)
